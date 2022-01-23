3a3f7de143a969d73d5ef89d5bcdb766ae245f11
IZerab <beltramelorenzo1999@gmail.com>
2022-01-23 18:45:05 +0100

    PA6
    Need to work on task 1.2, something strange in happening since the grid search does not predict well the labels in which I am interested

---
+++ PA6/Task_1.py
@@ -0,0 +1,188 @@
+# this is task 1 of Lorenzo Beltrame submission for Programming Assignment 6 UNIVIE WS2021
+
+# import standard libs
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+import pandas as pd
+
+
+def linear_kernel(a, b, parameter):
+    return np.dot(a, np.transpose(b))
+
+
+def gaussian_kernel(X, sigma):
+    """
+    Compute the kernel matrix
+    :param X: design matrix
+    :param sigma: normalization factor in the RBF function
+    :return: the kernel matrix
+    """
+    # get the norm of each element of X
+    X_norm = np.sum(X ** 2, axis= -1)
+
+    # get the kernel (decompose the norm in simpler operations!
+    K = np.exp(- (X_norm[:, None] + X_norm[None, :] - 2 * np.dot(X, X.T)) / (2 * sigma ** 2))
+    return K
+
+
+def custom_gaussian_kernel(X, hyperpar):
+    """
+    Compute the kernel matrix based on a modified RBF kernel
+    :param X: design matrix
+    :param hyperpar: tuple, normalization factor in the RBF function and a multiplicative factor
+    :return: the kernel matrix
+    """
+    # get the norm of each element of X
+    X_norm = np.sum(X ** 2, axis=-1)
+
+    # get the kernel (decompose the norm in simpler operations!
+    K = hyperpar[1] * np.exp(- (X_norm[:, None] + X_norm[None, :] - 2 * np.dot(X, X.T)) / (2 * hyperpar[0] ** 2))
+    return K
+
+
+def sigmoid(z):
+    """
+    Computes the sigmoid for a given input
+    :param z: float, input of the sigmoid
+    :return: the value of the input, it is mapped into [0, 1]
+    """
+    return np.exp(z) / (1 + np.exp(z))
+
+
+def log_reg_cost(K, y, alphas, Lambda):
+    """
+    Cost of the logistic regression
+    :param Lambda: regularization parameter
+    :param alphas: weight vector for the dual problem
+    :param K: kernel matrix
+    :param y: target vector
+    :return: the value of the cost function
+    """
+    arg = np.log(1 + np.exp(np.dot(alphas, K)))
+    first = np.sum(arg)
+    second = -np.dot(y, np.dot(alphas, K))
+    # regulariser
+    reg = Lambda * alphas.T @ K @ alphas
+
+    return first + second + reg
+
+
+def log_reg_gradient(K, y, alphas, Lambda):
+    """
+    Gradient of the loss function
+    :param Lambda: regularization parameter
+    :param K: kernel matrix
+    :param y: target vector
+    :param alphas: weight vector for the dual problem
+    :return: the gradient of the function (a vector!)
+    """
+    temp = -np.dot(K, y - sigmoid(np.dot(alphas, K)))
+    return temp + Lambda * alphas.T @ (K + K.T)
+
+
+class custom_log_reg:
+    """
+    This class implements logistic regression
+    """
+
+    def __init__(self, kernel='gaussian', hyper_kernel=None):
+        """
+        Initialize the function and save the train instances inside the class. Note that for this implementation only
+        the gaussian, customized gaussian and the linear and kernels are implemented.
+        If a custom kernel is chosen provide a tuple (sigma, var) where sigma is the hyperparameter of the gaussian
+        kernel and var is a multiplicative factor.
+        """
+        # class variables
+        self.alphas = None
+        self.K = None
+
+        if kernel == "linear":
+            self.kernel = linear_kernel
+            if hyper_kernel:
+                self.kern_param = hyper_kernel
+            else:
+                self.kern_param = 1
+        elif kernel == "gaussian":
+            self.kernel = gaussian_kernel
+            if hyper_kernel:
+                self.kern_param = hyper_kernel
+            else:
+                self.kern_param = 0.1
+        elif kernel == "custom":
+            self.kernel = custom_gaussian_kernel
+            if hyper_kernel:
+                self.kern_param = hyper_kernel
+            else:
+                self.kern_param = (0.1, 0.1)
+
+    def fit(self, X, y, lr=0.001, max_steps=100, Lambda=1, verbose=False):
+        """
+        This fuction fits the model to the training data.
+        :param max_steps: max number of iterations for the GD
+        :param lr: learning rate, a float
+        :param Lambda: regularization parameter
+        :param verbose: bool, wether or not print the descent of the cost
+        :param X: design matrix
+        :param y: target vector
+
+        :return:
+        """
+        # check if the input is a df or not and in case cast it to array
+        if isinstance(X, pd.DataFrame):
+            X = X.copy().to_numpy()
+        if isinstance(y, pd.Series):
+            y = y.copy().to_numpy()
+
+        m = X.shape[0]
+        # Construct kernel matrix
+        self.K = self.kernel(X, self.kern_param)
+
+        # Gradient descent
+        self.alphas = np.zeros([m])
+        costs = []
+
+        if verbose:
+            for j in range(max_steps):
+                current_lr = lr / (j+1)
+                self.alphas -= log_reg_gradient(self.K, y, self.alphas, Lambda=Lambda) * current_lr
+                costs.append(log_reg_cost(self.K, y, self.alphas, Lambda=Lambda))
+                if j % 200 == 0:
+                    print("The cost at the iteration {} is {}".format(j, costs[j]))
+
+        return costs
+
+    def predict_prob(self, X):
+        """
+        This function predicts the probability associated with a set of instances contained in X (the design matrix)
+        :param X: Design matrix
+        :return: a list with the probabilities inferred
+        """
+        # check if the input is a df or not and in case cast it to array
+        if isinstance(X, pd.DataFrame):
+            X = X.copy().to_numpy()
+
+        return sigmoid(np.dot(self.alphas, self.kernel(X, self.kern_param)))
+
+    def predict(self, X, threshold=0.5):
+        """
+        This function predicts the labels of a set of instances in a bi-classification problem.
+        If the probability is less than the threshold the value 0 is predicted, 1 otherwise
+        :param X: The design matrix
+        :param threshold: the threshold that is used to infer the label
+        :return: a list containing the labels
+        """
+        # predict the probability
+        Z = self.predict_prob(X)
+
+        # give the label
+        result = []
+        for pred in Z:
+            if pred < threshold:
+                result.append(0)
+            else:
+                result.append(1)
+
+        return result
+
